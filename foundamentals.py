f1fb81eca6a5fd7c3483ede04ad114a32a3fa5c0
Nicola De Rosa <nicderosa97@gmail.com>
2024-03-20 15:25:42 +0100

    Foundamentals

---
+++ foundamentals.py
@@ -0,0 +1,43 @@
+# Python Fundamentals
+
+# Variables and Data Types
+integer = 10
+floating_point = 20.5
+string = "Hello, World!"
+boolean = True
+
+# Lists
+list = [1, 2, 3, 4, 5]
+
+# Tuples
+tuple = (1, 2, 3, 4, 5)
+
+# Dictionaries
+dictionary = {"key": "value"}
+
+# Control Flow
+if boolean:
+    print("Boolean is True")
+else:
+    print("Boolean is False")
+
+# Loops
+for i in range(5):
+    print(i)
+
+# Functions
+def function():
+    print("This is a function")
+
+function()
+
+# Classes
+class MyClass:
+    def __init__(self):
+        self.variable = "I'm a variable in a class"
+
+    def method(self):
+        print("I'm a method in a class")
+
+my_class = MyClass()
+my_class.method()
